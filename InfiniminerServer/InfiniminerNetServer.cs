bdc81e0e7dd94d115686f5a2abb0c8699d4655f3
finoriko <finoriko@Hotmail.com>
2020-03-29 23:17:50 +0900

    초기 커밋.

---
+++ InfiniminerServer/InfiniminerNetServer.cs
@@ -0,0 +1,29 @@
+﻿using Lidgren.Network;
+using System.Linq;
+
+namespace Infiniminer
+{
+    public class InfiniminerNetServer : NetPeer
+    {
+        public InfiniminerNetServer(NetPeerConfiguration config)
+            : base(config)
+        {
+        }
+
+        /* crappy hack to fix duplicate key error crash in Lidgren, hopefully a new
+         * version of Lidgren will fix this issue. */
+        public bool SanityCheck(NetConnection connection)
+        {
+            if (this.Connections.Contains(connection) == false)
+            {
+                if (this.Connections.Contains(connection))
+                {
+                    this.Connections.Remove(connection);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ InfiniminerShared/DatafileLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Infiniminer
+{
+    public class DatafileLoader
+    {
+        Dictionary<string, string> dataDict = new Dictionary<string, string>();
+        public Dictionary<string, string> Data
+        {
+            get { return dataDict; }
+        }
+
+        public DatafileLoader(string filename)
+        {
+            try
+            {
+                FileStream file = new FileStream(filename, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(file);
+
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    string[] args = line.Split("=".ToCharArray());
+                    if (args.Length == 2 && line[0] != '#')
+                    {
+                        Data[args[0].Trim()] = args[1].Trim();
+                    }
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+                file.Close();
+            }
+            catch { }
+        }
+    }
+}
